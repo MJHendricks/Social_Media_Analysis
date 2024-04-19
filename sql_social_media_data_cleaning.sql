9e64b93cc08a1c83666dec5ec4ee81e2d0fec317
MJHendricks <matthew.joshua.hendricks@gmail.com>
2024-04-19 10:34:22 +0200

    modified:   social_media_data_clean_output.csv 	modified:   sql_social_media_data_cleaning.sql

--- sql_social_media_data_cleaning.sql
+++ sql_social_media_data_cleaning.sql
@@ -1,23 +1,4 @@
-SELECT TOP (1000) [Platform]
-      ,[Post ID]
-      ,[Post Type]
-      ,[Post Content]
-      ,[Post Timestamp]
-      ,[Likes]
-      ,[Comments]
-      ,[Shares]
-      ,[Impressions]
-      ,[Reach]
-      ,[Engagement Rate]
-      ,[Audience Age]
-      ,[Audience Gender]
-      ,[Audience Location]
-      ,[Audience Interests]
-      ,[Campaign ID]
-      ,[Sentiment]
-      ,[Influencer ID]
-  FROM [social].[dbo].[data_1$]
-
+  -- merge two datasets together
 SELECT * INTO Social_Media_Data
 FROM [data_1$];
 
@@ -39,6 +20,19 @@ SET "Date Posted" = CONVERT(DATE, [Post Timestamp]);
 UPDATE Social_Media_Data
 SET "Time Posted" = CONVERT(TIME, [Post Timestamp]);
 
+-- add a column for just the hours
+ALTER TABLE Social_Media_Data
+ADD "Hour Posted" INT;
+
+
+UPDATE Social_Media_Data
+SET "Hour Posted" = DATEPART(HOUR, [Time Posted]);
+
 -- Drop columns that i wont be using to improve performance
 ALTER TABLE Social_Media_Data
 DROP COLUMN "Post TimeStamp", "Audience Interests", "Campaign ID", "Sentiment", "Influencer ID";
+
+-- select only 2024 data for the viz
+SELECT * FROM Social_Media_Data
+WHERE DATEPART(YEAR, [Date Posted]) = '2024';
+
